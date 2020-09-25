9257ea1a612307106821132df407d70065271ced
mtalma <talmamateusz@gmail.com>
2020-09-25 23:02:13 +0200

    Delete Board constructor from test.cpp

--- test.cpp
+++ test.cpp
@@ -7,8 +7,6 @@
 #include "Board.hpp"
 #include "gtest/gtest.h"
 
-using Board = std::vector<std::vector<char>>;
-
 std::string toString(Board& br)
 {
     std::stringstream stm;
@@ -23,11 +21,6 @@ std::string toString(Board& br)
     return stm.str();
 }
 
-Board buildEmptyBoard(size_t size_x, size_t size_y)
-{
-    std::vector<std::vector<char>> vec(size_x, std::vector<char>(size_y, '.'));
-    return vec;
-}
 
 bool checkIfNumberInRow(std::vector<char>& input)
 {
