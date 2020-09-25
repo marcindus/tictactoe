d8c46c3b29aef2cf514fd3ebfc0b8a0d4752b93d
duszek <marcin.dus@gmail.com>
2020-09-26 00:24:53 +0200

    board ctor refactor

--- Board.cpp
+++ Board.cpp
@@ -5,8 +5,7 @@
 
 Board::Board(size_t sizeX_, size_t sizeY_) : sizeX(sizeX_), sizeY(sizeY_)
 {
-    std::vector<std::vector<char>> boardtmp_(sizeX_, std::vector<char>(sizeY_, '.'));
-    boardVec_ = boardtmp_;
+    boardVec_ = std::vector<std::vector<char>>(sizeX_, std::vector<char>(sizeY_, '.'));
 }
 
 
