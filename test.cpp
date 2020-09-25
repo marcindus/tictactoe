48df5271780b5a1b749e6f8020eb8752c6b9d1f5
duszek <marcin.dus@gmail.com>
2020-09-25 21:50:23 +0200

    Board constructor prototype, refactor needed

--- test.cpp
+++ test.cpp
@@ -1,12 +1,46 @@
 
+#include <vector>
+#include <sstream>
+#include <string>
+
 #include "Game.hpp"
 #include "Board.hpp"
 #include "gtest/gtest.h"
 
-TEST(Board, BoardSghouldDoSomething)
-{
-    int in = 1;
-    int out = 1;
+using Board = std::vector<std::vector<char>>;
 
-    EXPECT_EQ(in, out);
+
+
+std::string toString(Board& br)
+{
+    std::stringstream stm;
+    for (auto& row : br)
+    {
+        for (auto& col : row)
+        {
+            stm << col << " ";
+        }
+        stm << std::endl;
+    }
+    return stm.str();
+}
+
+Board build_empty_board(size_t size_x, size_t size_y)
+{
+    std::vector<std::vector<char> > vec( size_x , std::vector<char> (size_y, '.'));
+    return vec;
+}
+
+
+struct BoardTest : public ::testing::Test
+{
+    Board empty_board_3x3 = {{'.', '.', '.'}, {'.', '.', '.'}, {'.', '.', '.'}};
+};
+
+TEST_F(BoardTest, BoardSghouldDoSomething)
+{
+    Board br = build_empty_board(4,3);
+    std::cout << toString(br);
+
+    EXPECT_EQ(br, empty_board_3x3);
 }
